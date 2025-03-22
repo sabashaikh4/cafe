eae2cce92d53733c8968a21badddce3c4a334d3d
sabashaikh4 <sabahshaikh361@gmail.com>
2025-03-22 14:37:30 +0530

    Update UserService.java

--- src/main/java/com/sp/main/services/UserService.java
+++ src/main/java/com/sp/main/services/UserService.java
@@ -5,4 +5,19 @@ import com.sp.main.entities.User;
 public interface UserService {
 
 	public boolean Saveorders(User user);
+
+      // API creates a new user
+	public User createUser(User user);
+	
+	// get  user details 
+	public List<User> getAllUsers();
+	
+	//get user details by id
+	public Optional<User> getUserDetail(int id);
+	
+	// put (Update user detail)
+	public User updateUserDetail(int id , User user);
+	
+	// delete
+	public void deleteUser(int id);
 }
